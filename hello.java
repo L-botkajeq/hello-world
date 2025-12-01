20f04255a66db183b8d98e564ee18218a0ebe8ec
L-botkajeq <1245382393@qq.com>
2025-12-01 10:41:26 +0800

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,108 @@
+public class hello {
+    public static void main(String[] args) {
+        int x=9;
+        int y=10;
+        String name="asdc";
+        System.out.println(x+y);
+        System.out.println(name);
+         
+        String MyVariable = "这是一个有效的变量名";
+        int myVariable = 10;  // 大小写敏感，与上面的不同
+        double $price = 99.99;
+        boolean _isValid = true;
+        String sum_of_array = "数组求和";
+        int geeks123 = 123;
+        
+        System.out.println("=== 有效标识符示例 ===");
+        
+        System.out.println("MyVariable: "  + MyVariable);
+        
+        System.out.println("myVariable: " + myVariable);
+        
+        System.out.println("$price: " + $price);
+        
+        System.out.println("_isValid: " + _isValid);
+        
+        System.out.println("sum_of_array: " + sum_of_array);
+        
+        System.out.println("geeks123: " + geeks123);
+
+
+        
+
+        boolean b1=true;
+        boolean b2=false;
+        System.out.println("Is java fun "  +  b1);
+        System.out.println("Is fish tasty "  +  b2);
+
+        byte a=25;
+        byte t=-10;
+        System.out.println("Age:" + a);
+        System.out.println("temperature:" + t);
+
+        char g='A';
+        char s='$';
+        System.out.println("Grade: "  + g);
+        System.out.println("Symbol: " + s);
+
+        float pi=3.14f;
+        float gravity=9.81f;
+        System.out.println("Value of pi: " + pi);
+        System.out.println("Gravity: " + gravity);
+
+        int p=2000000;
+        int d=150000000;
+        System.out.println("Popluation: " + p + " people");
+        System.out.println("Distance " + d);
+
+        String n="Geek1";
+        String m="Hello, World!";
+        System.out.println("Name: " + n);
+        System.out.println("Message: " + m);
+        
+        double a1=3.141592653589793;
+        double an=6.02214076e23;
+        System.out.println("Value of a1: " + a1);
+        System.out.println("Avogadro's Number: " + an);
+
+
+        byte b =100;
+        short s2 =30000;
+        int i2 =100000;
+        long l2 =1000000000000L;
+
+        float f =3.14f;
+        double d2 =3.141592653589793;
+        
+        char c ='A';
+        boolean flag =true;
+
+        int [] arr={1,2,3,4,5};
+
+        System.out.println("arr[0]: " + arr[0]);
+        
+        System.out.println("=== Data Types Demo ===");
+        
+        System.out.println("Array: " +arr.length);
+
+        System.out.print("Array : " + arr[0]);
+
+        System.out.println("byte: " + b);
+
+        System.out.println("short: " + s2);
+
+        System.out.println("int: " + i2);
+
+        System.out.println("long: " + l2);
+
+        System.out.println("float: " + f);
+        
+        System.out.println("double: " + d2);
+        
+        System.out.println("char: " +c);
+        
+        System.out.println("flag: " + flag);
+
+
+    }
+}
